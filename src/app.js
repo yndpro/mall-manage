89180406e414f1930c3a40b5d67b94edf484e6ec
Adrian <yanglinde@4399inc.com>
2019-04-12 19:57:19 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Link,Switch} from 'react-router-dom';
+
+
+class App extends React.Component{
+    render() {
+        return <div>app</div>
+
+    }
+}
+
+
+
+export default App;
